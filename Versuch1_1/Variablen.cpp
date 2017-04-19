a30aee2f11a8a7609d7b5172be6e0d031c262f4e
nkpmedia <webmaster@nkp-media.de>
2017-04-19 17:20:06 +0200

    Daten und Typumwandlung 1

--- Versuch1_1/Variablen.cpp
+++ Versuch1_1/Variablen.cpp
@@ -11,6 +11,19 @@
 
 int main()
 {
+	int iErste = 0;
+	int iZweite = 0;
+
+	std::cout << "Geben sie eine Zahl ein: ";
+	std::cin >> iErste;
+	std::cout << "Geben sie eine Zahl ein: ";
+	std::cin >> iZweite;
+
+	int iSumme = iErste + iZweite;
+	int iQuotient = iErste / iZweite;
+
+	std::cout << "Die Summe ist: " << iSumme << std::endl;
+	std::cout << "Der Quotient ist: " << iQuotient << std::endl;
 	return 0;
 	
 }
