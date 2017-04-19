1a6f1370c8057e8b5379f43c2c558730e825397e
nkpmedia <webmaster@nkp-media.de>
2017-04-19 19:57:49 +0200

    Added task 2.2.1 and working cmakelist configuration

---
+++ Versuch2_1/fibonacci.cpp
@@ -0,0 +1,38 @@
+//
+// Created by paul on 19.04.17.
+//
+
+/*
+ * Formel für Fibonacci:
+ * f(0) = 0
+ * f(1) = 1
+ * f(b) = f(n-1) + f(n-2)
+ *
+ *
+ */
+
+#include <iostream>
+
+int fibonacci(int i);
+
+int main()
+{
+    for(int i = 0; i < 26 ; i++)
+    {
+        std::cout << fibonacci(i) << std::endl;
+    }
+    return 0;
+}
+
+/*!
+ * @brief Berechnet die Fibonacci-Zahl
+ * @param i Zu berechnede Fibonacci Zahl
+ * @return
+ *
+ * Schlechte Implementierung, da viel doppelt berechnet wird!!!
+ */
+int fibonacci(int i) {
+    if(i == 0) return 0;
+    else if(i==1) return 1;
+    else return fibonacci(i-1) + fibonacci(i-2);
+}
